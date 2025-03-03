7fc91a3991930de63ca75e7352f42e8d69b75f87
Paulo Ricardo <paulo.rdndc@gmail.com>
2025-03-03 18:20:03 -0300

    feat: Add routes

--- peopleManagement/start/routes.ts
+++ peopleManagement/start/routes.ts
@@ -1,16 +1,13 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+const ClientControler = () => import('#controllers/clients_controller')
+
+router
+  .group(() => {
+    router.get('/', [ClientControler, 'index'])
+    router.post('/', [ClientControler, 'create'])
+    router.get('/:id', [ClientControler, 'show'])
+    router.put('/:id', [ClientControler, 'update'])
+    router.delete('/:id', [ClientControler, 'destroy'])
+  })
+  .prefix('/clients')
