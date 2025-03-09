3d76224d20054220a338393c6f62e053e840bfa1
Paulo Ricardo <paulo.rdndc@gmail.com>
2025-03-09 20:52:24 -0300

    feat: Add a new feature of service

--- tools/app/services/gateway_service.ts
+++ tools/app/services/gateway_service.ts
@@ -23,29 +23,20 @@ export default class GatewayService {
         response: response.data,
       }
     } catch (error) {
-      Logger.error(`Gateway1 falhou: ${error.message}`)
-      throw new Error('Gateway1 falhou')
+      Logger.error(`Gateway1 failed: ${error.message}`)
+      throw new Error('Gateway1 failed')
     }
   }
 
   private async processGateway2(payload: any) {
     try {
-      const response = await axios.post(
-        `${this.gateway2Url}/transacoes`,
-        {
-          valor: payload.amount,
-          nome: payload.name,
-          email: payload.email,
-          numeroCartao: payload.cardNumber,
-          cvv: payload.cvv,
-        }
-        // {
-        //   headers: {
-        //     'Gateway-Auth-Token': 'tk_f2198cc671b5289fa856',
-        //     'Gateway-Auth-Secret': '3d15e8ed6131446ea7e3456728b1211f',
-        //   },
-        // }
-      )
+      const response = await axios.post(`${this.gateway2Url}/transacoes`, {
+        valor: payload.amount,
+        nome: payload.name,
+        email: payload.email,
+        numeroCartao: payload.cardNumber,
+        cvv: payload.cvv,
+      })
 
       return {
         success: true,
@@ -54,17 +45,95 @@ export default class GatewayService {
         response: response.data,
       }
     } catch (error) {
-      Logger.error(`Gateway2 falhou: ${error.message}`)
-      throw new Error('Gateway2 falhou')
+      Logger.error(`Gateway2 failed: ${error.message}`)
+      throw new Error('Gateway2 failed')
     }
   }
 
-  public async processPayment(payload: any) {
+  private async getAllTransactionsFromGateway1() {
+    try {
+      const response = await axios.get(`${this.gateway1Url}/transactions`)
+      return {
+        success: true,
+        gateway: 'Gateway1',
+        data: response.data,
+      }
+    } catch (error) {
+      Logger.error(`Error fetching transactions from Gateway1: ${error.message}`)
+      throw new Error('Error fetching transactions from Gateway1')
+    }
+  }
+
+  private async getAllTransactionsFromGateway2() {
+    try {
+      const response = await axios.get(`${this.gateway2Url}/transacoes`)
+      return {
+        success: true,
+        gateway: 'Gateway2',
+        data: response.data,
+      }
+    } catch (error) {
+      Logger.error(`Error fetching transactions from Gateway2: ${error.message}`)
+      throw new Error('Error fetching transactions from Gateway2')
+    }
+  }
+
+  private async processChargebackGateway1(transactionId: string) {
+    try {
+      const response = await axios.post(
+        `${this.gateway1Url}/transactions/${transactionId}/charge_back`
+      )
+      return {
+        success: true,
+        gateway: 'Gateway1',
+        data: response.data,
+      }
+    } catch (error) {
+      Logger.error(`Error processing refund in Gateway1: ${error.message}`)
+      throw new Error('Refund not processed in Gateway1')
+    }
+  }
+
+  private async processChargebackGateway2(transactionId: string) {
+    try {
+      const response = await axios.post(`${this.gateway2Url}/transacoes/reembolso`, {
+        id: transactionId,
+      })
+      return {
+        success: true,
+        gateway: 'Gateway2',
+        data: response.data,
+      }
+    } catch (error) {
+      Logger.error(`Error processing refund in Gateway2: ${error.message}`)
+      throw new Error('Refund not processed in Gateway2')
+    }
+  }
+
+  async processPayment(payload: any) {
     try {
       return await this.processGateway1(payload)
     } catch (error) {
-      Logger.info('Tentando Gateway2 após falha no Gateway1')
+      Logger.info('Trying Gateway2 after Gateway1 failure')
       return await this.processGateway2(payload)
+    }
+  }
+
+  async getAllTransactions() {
+    try {
+      return await this.getAllTransactionsFromGateway1()
+    } catch (error) {
+      Logger.info('Trying Gateway2 after Gateway1 failure')
+      return await this.getAllTransactionsFromGateway2()
+    }
+  }
+
+  async processChargeback(transactionId: string) {
+    try {
+      return await this.processChargebackGateway1(transactionId)
+    } catch (error) {
+      Logger.info('Trying Gateway2 after Gateway1 failure')
+      return await this.processChargebackGateway2(transactionId)
     }
   }
 }
