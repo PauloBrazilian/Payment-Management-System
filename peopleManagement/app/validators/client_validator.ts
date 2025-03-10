64318c4ffb7756dd8cd603d7081cee67fe7dc108
Paulo Ricardo <paulo.rdndc@gmail.com>
2025-03-09 21:22:44 -0300

    fix: More validation in email

--- peopleManagement/app/validators/client_validator.ts
+++ peopleManagement/app/validators/client_validator.ts
@@ -3,6 +3,6 @@ import vine from '@vinejs/vine'
 export const clientValidator = vine.compile(
   vine.object({
     name: vine.string().trim().minLength(1).maxLength(256),
-    email: vine.string().trim().email(),
+    email: vine.string().trim().email().normalizeEmail(),
   })
 )
